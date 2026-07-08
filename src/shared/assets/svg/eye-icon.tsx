65259ce4056d54f37e0c0cead03c04b98b05dfe6
Maxim D <renissance@tut.by>
2026-07-08 18:47:32 +0300

    feat: add icons

---
+++ src/shared/assets/svg/eye-icon.tsx
@@ -0,0 +1,31 @@
+import Svg, { Circle, Path } from 'react-native-svg';
+
+type PropsType = {
+  color?: string;
+  size?: number;
+  strokeWidth?: number;
+};
+
+export const EyeIcon = (props: PropsType) => {
+  const { color = '#000000', size = 24, strokeWidth = 2 } = props;
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <Path
+        stroke={color}
+        strokeWidth={strokeWidth}
+        d="M2.062 12.348a1 1 0 010-.696 10.75 10.75 0 0119.876 0 1 1 0 010 .696 10.75 10.75 0 01-19.876 0"
+      />
+      <Circle cx={12} cy={12} r={3} />
+    </Svg>
+  );
+};
---
+++ src/shared/assets/svg/eye-off-icon.tsx
@@ -0,0 +1,35 @@
+import Svg, { Path } from 'react-native-svg';
+
+type PropsType = {
+  color?: string;
+  size?: number;
+  strokeWidth?: number;
+};
+
+export const EyeOffIcon = (props: PropsType) => {
+  const { color = '#000000', size = 24, strokeWidth = 2 } = props;
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <Path
+        stroke={color}
+        strokeWidth={strokeWidth}
+        d="M10.733 5.076a10.744 10.744 0 0111.205 6.575 1 1 0 010 .696 10.747 10.747 0 01-1.444 2.49M14.084 14.158a3 3 0 01-4.242-4.242"
+      />
+      <Path
+        stroke={color}
+        strokeWidth={strokeWidth}
+        d="M17.479 17.499a10.75 10.75 0 01-15.417-5.151 1 1 0 010-.696 10.75 10.75 0 014.446-5.143M2 2l20 20"
+      />
+    </Svg>
+  );
+};
